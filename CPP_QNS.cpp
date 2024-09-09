b4fb6087f9cc52b5408c87fe78f087a11b6d036f
ArnavKaul <121275829+ArnavKaul@users.noreply.github.com>
2024-09-09 23:09:15 +0530

    Ht

---
+++ CPP_QNS.cpp
@@ -0,0 +1,18 @@
+
+//Q2
+// float calculate float radius, float height
+
+//     float area;
+//     area = 3.14 * radius*radius * height;
+//     cout<<area;
+//     return area;
+
+// int main() {
+//     float area,radius,height;
+//     cin>>radius;
+//     cin>>height;
+//     calculate(radius,height);
+ 
+//     return 0;
+// }
+
