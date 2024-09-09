7befba0d8a059fcfbf366dbe9ae1aa5b2819fa50
Tanush Krashak <139574093+TanushKrashak@users.noreply.github.com>
2024-09-09 23:15:12 +0530

    Create Q2.cpp

---
+++ Q2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// Q2
+
+int main()
+{
+    int num = 1;
+    while (num < 10) {
+        cout << num;
+        num = num - 1;
+    }
+    return 0;
+}
